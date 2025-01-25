fc03706a739c80c05addca95f5bd43ecc6f77341
Roland Meertens <rolandmeertens@gmail.com>
2025-01-25 22:20:37 +0000

    ⭐️Version bump and rename of the running node

--- launch/viam_rover_launch.py
+++ launch/viam_rover_launch.py
@@ -6,89 +6,49 @@ def generate_launch_description():
         Node(
             package='viam_rover_package',
             namespace='viam_rover',
-            executable='viam_rover_encoder',
-            name='viam_rover_enc'
-        ),
-        Node(
-            package='foxglove_bridge',
-            namespace='foxglove_bridge',
-            executable='foxglove_bridge',
-            name='foxglove_bridge'
+            executable='viam_rover_control',
+            name='viam_rover_control'
         ),
         # Node(
-        #     package='usb_cam',
-        #     namespace='usb_cam',
-        #     executable='usb_cam_node_exe',
-        #     arguments=['--params-file', '/home/roland/ros2_ws/src/viam_rover_package/config/usb_cam.yaml'],
-        #     name='usb_cam'
+        #     package='foxglove_bridge',
+        #     namespace='foxglove_bridge',
+        #     executable='foxglove_bridge',
+        #     name='foxglove_bridge'
         # ),
+        # # Node(
+        # #     package='usb_cam',
+        # #     namespace='usb_cam',
+        # #     executable='usb_cam_node_exe',
+        # #     arguments=['--params-file', '/home/roland/ros2_ws/src/viam_rover_package/config/usb_cam.yaml'],
+        # #     name='usb_cam'
+        # # ),
         # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     arguments=['0.2', '0.0', '0.3', '1.57', '3.14', '1.57', 'base_link', 'default_cam'],
-        #     name='static_transform_publisher'
+        #     package='robot_state_publisher',
+        #     executable='robot_state_publisher',
+        #     name='robot_state_publisher',
+        #     output='screen',
+        #     arguments=['/home/roland/ros2_ws/src/viam-rover-ros/description/viam_rover_description.urdf']
         # ),
-        # Urdf 
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            arguments=['/home/roland/ros2_ws/src/viam-rover-ros/description/viam_rover_description.urdf']
-        ),
 
         # Node(
         #     package='tf2_ros',
         #     executable='static_transform_publisher',
-        #     name='static_tf_pub',
-        #     arguments=['0', '0', '0', '0', '0', '0', '1', 'map', 'odom'],
+        #     name='some_publisher',
+        #     arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_footprint'],
         #     output='screen',
         # ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='some_publisher',
-            arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_footprint'],
-            output='screen',
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='some_publisher_2',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_footprint', 'base_link'],
-            output='screen',
-        ),
-         Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='some_publisher_3',
-            arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom'],
-            output='screen',
-        ),
         # Node(
         #     package='tf2_ros',
         #     executable='static_transform_publisher',
         #     name='some_publisher_2',
-        #     arguments=['0', '0', '0', '0', '0', '0', '0', 'base_footprint', 'base_link'],
+        #     arguments=['0', '0', '0', '0', '0', '0', 'base_footprint', 'base_link'],
         #     output='screen',
         # ),
-        # Node(
+        #  Node(
         #     package='tf2_ros',
         #     executable='static_transform_publisher',
         #     name='some_publisher_3',
-        #     arguments=['0', '0', '0', '0', '0', '0', '0', 'map', 'odom'],
+        #     arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom'],
         #     output='screen',
         # ),
-        
-#ros2 run nav2_map_server map_server /home/roland/ros2_ws/src/viam-rover-ros/map/map.yaml
-        # Node(
-        #     package='nav2_map_server',
-        #     executable='map_server',
-        #     name='map_server',
-        #     output='screen',
-        #     parameters=[
-        #         {'yaml_filename': '/home/roland/ros2_ws/src/viam-rover-ros/map/map.yaml'}
-        #     ]
-        #     # arguments=['/home/roland/ros2_ws/src/viam-rover-ros/map/map.yaml']
-        # )
     ])
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'viam_rover_package'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,15 +13,14 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='roland',
-    maintainer_email='roland@todo.todo',
-    description='TODO: Package description',
+    maintainer='Roland Meertens',
+    maintainer_email='rolandmeertens@gmail.com',
+    description='ROS2 package for the VIAM rover',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'viam_rover_encoder = viam_rover_package.viam_rover_encoder:main',
-            # 'viam_rover_motor_control = viam_rover_package.viam_rover_motor_control:main'
+            'viam_rover_control = viam_rover_package.viam_rover_control:main',
         ],
     },
 )
